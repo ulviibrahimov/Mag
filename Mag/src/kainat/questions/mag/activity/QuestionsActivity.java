15ce36987bc47934ddb1157a5033d6e7b0961447
ulviibrahimov <ulvi@ualberta.ca>
2013-12-21 02:14:02 -0600

    1st question displayed

--- Mag/src/kainat/questions/mag/activity/QuestionsActivity.java
+++ Mag/src/kainat/questions/mag/activity/QuestionsActivity.java
@@ -1,6 +1,5 @@
 package kainat.questions.mag.activity;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,6 +9,8 @@ import kainat.questions.mag.model.Question;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class QuestionsActivity extends Activity {
 
@@ -20,17 +21,8 @@ public class QuestionsActivity extends Activity {
 		ParseQuestions test=new ParseQuestions(this.getApplicationContext());
 		List<Question> questionL= new ArrayList<Question>();
 		questionL=test.questions("questions.xml");
-		for(int i=0;i<questionL.size();i++){
-			System.out.println(questionL.get(i).getQuestionText());
-			System.out.println(questionL.get(i).getChoiceA());
-			System.out.println(questionL.get(i).getChoiceB());
-			System.out.println(questionL.get(i).getChoiceC());
-			System.out.println(questionL.get(i).getChoiceD());
-			System.out.println(questionL.get(i).getChoiceE());
-			System.out.println(questionL.get(i).getCorrectAnswer());
-			System.out.println(questionL.get(i).getQuestionType());
-			System.out.println(questionL.get(i).getImage());
-		}
+		this.displayQuestion(questionL.get(0), 0);
+		
 	}
 
 	@Override
@@ -38,6 +30,29 @@ public class QuestionsActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.questions, menu);
 		return true;
+	}
+	
+	public void displayQuestion(Question question, Integer index){
+		TextView t=new TextView(this);
+		t=(TextView)findViewById(R.id.textView2);
+		t.setText("Sual: "+(index+1));
+		t=(TextView)findViewById(R.id.textView1);
+		t.setText(question.getQuestionText());
+		Button aButton=(Button)findViewById(R.id.abutton);
+        aButton.setText(question.getChoiceA());
+        aButton.setTextSize(10);
+        
+        Button bButton=(Button)findViewById(R.id.bbutton);
+        bButton.setText(question.getChoiceB());
+        
+        Button cButton=(Button)findViewById(R.id.cbutton);
+        cButton.setText(question.getChoiceC());
+        
+        Button dButton=(Button)findViewById(R.id.dbutton);
+        dButton.setText(question.getChoiceD());
+        
+        Button eButton=(Button)findViewById(R.id.ebutton);
+        eButton.setText(question.getChoiceE());
 	}
 
 }
