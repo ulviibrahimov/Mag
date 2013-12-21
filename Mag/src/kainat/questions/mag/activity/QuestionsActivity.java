324c89b134c4743abc8e0b7498085094d8360d48
ulviibrahimov <ulvi@ualberta.ca>
2013-12-21 08:01:00 -0600

    buttons working

--- Mag/src/kainat/questions/mag/activity/QuestionsActivity.java
+++ Mag/src/kainat/questions/mag/activity/QuestionsActivity.java
@@ -8,6 +8,7 @@ import kainat.questions.mag.controller.ParseQuestions;
 import kainat.questions.mag.model.Question;
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -19,6 +20,12 @@ import android.widget.Toast;
 public class QuestionsActivity extends Activity {
 	List<Question> questionL= new ArrayList<Question>();
 	Integer questionIndex=0;
+	Context context=this;
+	Button aButton;
+	Button bButton;
+	Button cButton;
+	Button dButton;
+	Button eButton;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -28,6 +35,11 @@ public class QuestionsActivity extends Activity {
 		this.displayQuestion(questionL.get(0), 0);
 		addListenerOnButtonPrev();
 		addListenerOnButtonNext();
+		addListenerOnButtonA();
+		addListenerOnButtonB();
+		addListenerOnButtonC();
+		addListenerOnButtonD();
+		addListenerOnButtonE();
 	}
 
 	@Override
@@ -101,4 +113,123 @@ public class QuestionsActivity extends Activity {
 		});
  
 	}	
+	
+	public void addListenerOnButtonA() {
+		 
+		aButton = (Button) findViewById(R.id.abutton);
+ 
+		aButton.setOnClickListener(new OnClickListener() {
+ 
+			@Override
+			public void onClick(View arg0) {
+				if(questionL.get(questionIndex).getCorrectAnswer().equals("a")){
+					Toast toast=Toast.makeText(context, "Cavabınız düzgündür!", 1);
+					toast.getView().setBackgroundColor(Color.GREEN);
+					toast.show();
+				}
+				else{
+					Toast toast=Toast.makeText(context, "Cavabınız səhvdir!", 1);
+					toast.getView().setBackgroundColor(Color.RED);
+					toast.show();
+				}
+			}
+ 
+		});
+ 
+	}	
+	public void addListenerOnButtonB() {
+		 
+		bButton = (Button) findViewById(R.id.bbutton);
+ 
+		bButton.setOnClickListener(new OnClickListener() {
+ 
+			@Override
+			public void onClick(View arg0) {
+				if(questionL.get(questionIndex).getCorrectAnswer().equals("b")){
+					Toast toast=Toast.makeText(context, "Cavabınız düzgündür!", 1);
+					toast.getView().setBackgroundColor(Color.GREEN);
+					toast.show();
+				}
+				else{
+					Toast toast=Toast.makeText(context, "Cavabınız səhvdir!", 1);
+					toast.getView().setBackgroundColor(Color.RED);
+					toast.show();
+				}
+			}
+ 
+		});
+ 
+	}
+	
+	public void addListenerOnButtonC() {
+		 
+		cButton = (Button) findViewById(R.id.cbutton);
+ 
+		cButton.setOnClickListener(new OnClickListener() {
+ 
+			@Override
+			public void onClick(View arg0) {
+				if(questionL.get(questionIndex).getCorrectAnswer().equals("c")){
+					Toast toast=Toast.makeText(context, "Cavabınız düzgündür!", 1);
+					toast.getView().setBackgroundColor(Color.GREEN);
+					toast.show();
+				}
+				else{
+					Toast toast=Toast.makeText(context, "Cavabınız səhvdir!", 1);
+					toast.getView().setBackgroundColor(Color.RED);
+					toast.show();
+				}
+			}
+ 
+		});
+ 
+	}
+	
+	public void addListenerOnButtonD() {
+		 
+		dButton = (Button) findViewById(R.id.dbutton);
+ 
+		dButton.setOnClickListener(new OnClickListener() {
+ 
+			@Override
+			public void onClick(View arg0) {
+				if(questionL.get(questionIndex).getCorrectAnswer().equals("d")){
+					Toast toast=Toast.makeText(context, "Cavabınız düzgündür!", 1);
+					toast.getView().setBackgroundColor(Color.GREEN);
+					toast.show();
+				}
+				else{
+					Toast toast=Toast.makeText(context, "Cavabınız səhvdir!", 1);
+					toast.getView().setBackgroundColor(Color.RED);
+					toast.show();
+				}
+			}
+ 
+		});
+ 
+	}
+	
+	public void addListenerOnButtonE() {
+		 
+		eButton = (Button) findViewById(R.id.ebutton);
+ 
+		eButton.setOnClickListener(new OnClickListener() {
+ 
+			@Override
+			public void onClick(View arg0) {
+				if(questionL.get(questionIndex).getCorrectAnswer().equals("e")){
+					Toast toast=Toast.makeText(context, "Cavabınız düzgündür!", 1);
+					toast.getView().setBackgroundColor(Color.GREEN);
+					toast.show();
+				}
+				else{
+					Toast toast=Toast.makeText(context, "Cavabınız səhvdir!", 1);
+					toast.getView().setBackgroundColor(Color.RED);
+					toast.show();
+				}
+			}
+ 
+		});
+ 
+	}
 }
